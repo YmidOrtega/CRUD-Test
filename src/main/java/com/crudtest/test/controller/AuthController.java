90076ece8749fac0ffee56151e3713d45efc236a
Ymid Ortega <yortegap7920@gmail.com>
2025-04-18 01:02:21 -0500

    Update AuthController and User model

--- src/main/java/com/crudtest/test/controller/AuthController.java
+++ src/main/java/com/crudtest/test/controller/AuthController.java
@@ -7,6 +7,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -19,7 +20,7 @@ public class AuthController {
     }
 
     @PostMapping ("/login")
-    public ResponseEntity <AuthUserDTO> AuthUserString (@Valid AuthUserDTO authUserDTO) {
+    public ResponseEntity <AuthUserDTO> AuthUserString (@RequestBody @Valid AuthUserDTO authUserDTO) {
         Authentication token = new UsernamePasswordAuthenticationToken(
                 authUserDTO.email(),
                 authUserDTO.password()
--- src/main/java/com/crudtest/test/model/User.java
+++ src/main/java/com/crudtest/test/model/User.java
@@ -3,6 +3,7 @@ package com.crudtest.test.model;
 import jakarta.persistence.*;
 import lombok.*;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.time.LocalDate;
@@ -43,7 +44,7 @@ public class User implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of();
+        return List.of(new SimpleGrantedAuthority(roleId.getName()));
     }
 
     @Override
