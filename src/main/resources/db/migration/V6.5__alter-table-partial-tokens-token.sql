83e87796a6efff0fb0ac4a79305715a9f1b0c13e
Ymid Ortega <yortegap7920@gmail.com>
2025-05-14 21:09:46 -0500

    Update user controller and test

--- src/main/resources/db/migration/V6.5__alter-table-partial-tokens-token.sql
+++ src/main/resources/db/migration/V6.5__alter-table-partial-tokens-token.sql
@@ -1,2 +1,1 @@
-ALTER TABLE partial_tokens MODIFY token VARCHAR(1024);
-
+ALTER TABLE partial_tokens MODIFY token VARCHAR(768);
--- src/test/java/com/crudtest/test/module/user/controller/UserControllerTest.java
+++ src/test/java/com/crudtest/test/module/user/controller/UserControllerTest.java
@@ -8,9 +8,7 @@ import org.springframework.boot.test.autoconfigure.json.AutoConfigureJsonTesters
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.json.JacksonTester;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
